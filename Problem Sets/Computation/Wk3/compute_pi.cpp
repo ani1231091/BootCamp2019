7beb34758723eea713aa8c9a02eb200baafe6d3c
GhoshA <ani1231091@gmail.com>
2019-07-28 13:57:20 -0500

    Add files via upload

---
+++ Problem Sets/Computation/Wk3/compute_pi.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int i;
+    double x, pi, sum = 0.0;
+    int N;
+    double step;
+
+    N = 100000;
+
+    step = 1.0 / (double) N;
+
+    for (i=0; i < N; i++){
+
+        x = (i + .5) * step;
+        sum = sum + 4.0 /(1.0 + x*x);
+    }
+
+    pi = step * sum;
+
+    cout << "Pi = " << pi << endl;
+    
+    return 0;
+}
+
